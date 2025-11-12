5a47b2ec107d013920fb7c43d205827532cb122e
VICTOR GREGORIO <victorhugogrey@gmail.com>
2025-11-12 16:29:40 -0300

    Update rodar_conciliacao.py

--- Cleaner-Henrique/rodar_conciliacao.py
+++ Cleaner-Henrique/rodar_conciliacao.py
@@ -3,7 +3,7 @@ import re
 import os
 import sys
 import io 
-from firebase_utils import get_db # <-- Importa nossa nova função
+from .firebase_utils import get_db # <-- ADICIONE O PONTO
 
 # --- Imports da função robusta (XML) ---
 import xml.etree.ElementTree as ET
@@ -404,3 +404,4 @@ def rodar_conciliacao_streamlit(caminho_arquivo_xml):
     else:
         output_stream.seek(0)
         return output_stream, False
+
